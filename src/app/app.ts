17eeffacd899edc5b8c94b05a8a2b82039ab9f29
Reichsgraf <graf.von.gers@gmail.com>
2026-04-18 18:03:58 +0300

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,16 @@
 import {Component, Inject, LOCALE_ID, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import {MAT_DATE_LOCALE, provideNativeDateAdapter} from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatTimepickerModule} from '@angular/material/timepicker';
 import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {MatButton} from '@angular/material/button';
 import { Clipboard } from '@angular/cdk/clipboard';
 import {formatDate, DecimalPipe} from '@angular/common';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { MatDivider } from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
@@ -18,17 +18,14 @@ import { MatDivider } from '@angular/material/divider';
     RouterOutlet,
     MatFormFieldModule,
     MatInputModule,
-    MatDatepickerModule,
     ReactiveFormsModule,
     MatButton,
-    MatTimepickerModule,
     MatAutocompleteModule,
     MatDivider,
-    DecimalPipe
-  ],
-  providers: [
-    provideNativeDateAdapter(),
-    { provide: MAT_DATE_LOCALE, useValue: 'uk-UA' }
+    MatCardModule,
+    DecimalPipe,
+    MatExpansionModule,
+    MatTooltipModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
@@ -46,11 +43,12 @@ export class App implements OnInit {
     const currentDate: Date = new Date();
 
     this.formGroup = new FormGroup({
-      startPointHeight: new FormControl(''),
-      riftPointHeight: new FormControl(''),
-      riftPointDistance: new FormControl(''),
-      targetPointHeight: new FormControl(''),
-      targetPointDistance: new FormControl(''),
+      startPointHeight: new FormControl('175'),
+      startPointDistance: new FormControl('0'),
+      riftPointHeight: new FormControl('170'),
+      riftPointDistance: new FormControl('14'),
+      targetPointHeight: new FormControl('140'),
+      targetPointDistance: new FormControl('16'),
     });
   }
 
@@ -64,12 +62,13 @@ export class App implements OnInit {
 
   calculate() {
     this.launched = true;
+    const x0 = this.formGroup.get("startPointDistance")?.value?.replace(",", ".");
     const y0 = this.formGroup.get("startPointHeight")?.value?.replace(",", ".");
     const x1 = this.formGroup.get("riftPointDistance")?.value.replace(",", ".");
     const y1 = this.formGroup.get("riftPointHeight")?.value.replace(",", ".");
     const x2 = this.formGroup.get("targetPointDistance")?.value.replace(",", ".");
     const y2 = this.formGroup.get("targetPointHeight")?.value.replace(",", ".");
 
-    this.result = ((-1) * (y2 - y1) / this.precise(x2 - x1) * x1 + Number(y1) - y0);
+    this.result = (y2 - y1) / this.precise(x2 - x1) * (x0 - x1) + Number(y1) - y0;
   }
 }
